a5a9a066da5b9f7dbdfaa3c47c226bfeec0bfa2e
eren23 <erenak1996@gmail.com>
2020-09-05 15:03:33 +0300

    register form added

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/core'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+
+const Index = () => (
+  <div>Hello world!</div>
+)
+
+export default Index
