bc310e3db0c6596fc5367bc1860ced36310696a4
eren23 <erenak1996@gmail.com>
2020-09-06 12:59:58 +0300

    forgot password added

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,6 +9,8 @@ import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from "next/router"
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
 interface registerProps {
 
 }
@@ -19,8 +21,8 @@ const Register: React.FC<registerProps> = ({ }) => {
     const [{ }, register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
-            <Formik initialValues={{ username: "", password: "" }} onSubmit={async (values, { setErrors }) => {
-                const response = await register(values)
+            <Formik initialValues={{ email: "", username: "", password: "" }} onSubmit={async (values, { setErrors }) => {
+                const response = await register({ options: values })
                 if (response.data?.register.errors) {
                     setErrors(toErrorMap(response.data.register.errors))
                 } else if (response.data?.register.user) {
@@ -36,9 +38,14 @@ const Register: React.FC<registerProps> = ({ }) => {
                                 label="Username"
                             />
                             <Box mt={4}>
+                                <InputField name="email" placeholder="email" label="Email" />
+
+                            </Box>
+                            <Box mt={4}>
                                 <InputField name="password" placeholder="password" label="Password" type="password" />
 
                             </Box>
+
                             <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">Register</Button>
                         </Form>
                     )
@@ -48,4 +55,4 @@ const Register: React.FC<registerProps> = ({ }) => {
     );
 }
 
-export default Register
+export default withUrqlClient(createUrqlClient)(Register);
