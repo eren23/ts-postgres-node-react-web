3fc28f041ec90852ee2ab7f49a558a042bc1efc9
eren23 <erenak1996@gmail.com>
2020-09-08 18:15:53 +0300

    main page ui changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,44 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Link } from "@chakra-ui/core";
+import { Link, Stack, Box, Heading, Text, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link"
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data, fetching }] = usePostsQuery({
+    variables: {
+      limit: 10
+    }
+  });
+  if (!fetching && !data) {
+    return <div>You got no data</div>
+  }
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>
-          Create a Post
-      </Link>
-      </NextLink>
-      <div>hello world</div>
+      <Flex align="center">
+        <Heading >LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">
+            Create a Post
+           </Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {fetching && !data ? (
         <div>loading...</div>
-      ) : (
-          data.posts.map((p) => <div key={p.id}>{p.title} </div>)
+      ) : (<Stack spacing={8}>
+        {data!.posts.map((p) => (
+
+          <Box key={p.id} p={5} shadow="md" borderWidth="1px" >
+
+            <Heading fontSize="xl">{p.title}</Heading>
+            <Text mt={4}>{p.textSnippet}</Text>
+          </Box>
+        ))}
+      </Stack>
         )}
+      {data ? <Flex> <Button isLoading={fetching} m="auto" my={4} > Load More </Button></Flex> : null}
+
     </Layout>
   );
 };
